545b6021e0ff4f0fc62de70db79863bfc93c9eb9
Kay Marquardt (Gnadelwartz) <kay@rrr.de>
2020-11-15 18:26:33 +0100

    integrate curl2cookie into curl.js

--- curl.js
+++ curl.js
@@ -278,4 +278,49 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// converts a sting containing netscape cookies to an Array
+// retrun false if curl or wget signature is not detected
+function curl2cookies(text) {
+	
+	// split text into lines
+	var cookies = [];
+	var lines = text.split("\n");
 
+	if (! lines[0].toLowerCase().includes("http cookie file")) { return false; }
+ 
+	// iterate over lines
+	lines.forEach(function(line, index){
+		// split lines imto values
+		var tokens = line.split("\t");
+ 
+		// we only care for valid cookie def lines
+		if (tokens.length == 7) {
+			// trim the tokens
+			tokens = tokens.map(function(e){return e.trim();});
+			var cookie = {};
+			cookie.httpOnly = 'false'; 
+			// Extract the data
+			cookie.domain = tokens[0];
+			if (tokens[0].startsWith("#HttpOnly_")) {
+				cookie.domain = tokens[0].replace("#HttpOnly_", '')
+				cookie.httpOnly = 'true'; 
+			}
+			cookie.flag = tokens[1] === 'TRUE';
+			cookie.path = tokens[2];
+			cookie.secure = tokens[3] === 'TRUE';
+ 
+			// Convert date to a readable format
+			var timestamp = tokens[4];
+			if (timestamp.length == 17){
+				timestamp = Math.floor(timestamp / 1000000 - 11644473600);
+			}
+			cookie.expiration = timestamp;
+			cookie.name = tokens[5];
+			cookie.value = tokens[6];
+			// Record the cookie.
+			cookies.push(cookie);
+		}	
+	});
+	
+	return cookies;
+}
